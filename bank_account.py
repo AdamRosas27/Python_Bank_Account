e95389435354d5380c412df22032b2c885ef3cf0
Adam Rosas <adamrosas27@gmail.com>
2022-09-06 17:31:00 -0700

    add error class and valueTooSmallError class

--- bank_account.py
+++ bank_account.py
@@ -1,14 +1,22 @@
+class Error(Exception):
+    pass
+
+
+class ValueTooSmallError(Error):
+    pass
+
+
 class BankAccount:
-    # represents starting balance of a new bank account
+    # Represents starting balance of a new bank account
     balance = 0
 
-    # name user types will be assigned to bank account object that will be created
+    # Name user types will be assigned to bank account object that will be created
     def __init__(self, name):
         self.name = name
 
     # Returns bank account info if user prints bank account object
     def __repr__(self):
-        return "%s's account. Current Balance: $%.2f" % (self.name, self.balance)
+        return "%s's Account. Current Balance: $%.2f" % (self.name, self.balance)
 
     def show_balance(self):
         print("Current Balance: $%.2f" % (self.balance))
